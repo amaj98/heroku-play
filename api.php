478f4609d1e8b74ada34125e3f71c4c8ba4489e4
amaj98 <amaj1998@gmail.com>
2019-09-23 17:45:50 -0400

    test

--- api.php
+++ api.php
@@ -3,7 +3,16 @@
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
- 
+    
+    //this part is perhaps overkill but I wanted to set the HTTP headers and status code
+    //making to this line means everything was great with this request
+    header('HTTP/1.1 200 OK');
+    //this lets the browser know to expect json
+    header('Content-Type: application/json');
+    //this creates json and gives it back to the browser
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
     //this is a sample query which gets some data, the order by part shuffles the results
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
@@ -20,13 +29,7 @@
     //I chose to get associative arrays inside of a big array
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    
-    //this part is perhaps overkill but I wanted to set the HTTP headers and status code
-    //making to this line means everything was great with this request
-    header('HTTP/1.1 200 OK');
-    //this lets the browser know to expect json
-    header('Content-Type: application/json');
-    //this creates json and gives it back to the browser
     echo json_encode($results);
+    }
 
 ?>
